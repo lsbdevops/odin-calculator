3d372ec6b4084924085e9d03f44b6896d873dded
lsbdevops <lsbdevops@gmail.com>
2023-09-08 09:29:14 +1000

    Create operate function which returns calc result
    Operator function checks the operator character and applies the
    associated with mathematical operation (by function call), returning
    the result. This function will be used to return the user a result
    for their input.

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(x, y) {
 function divide(x, y) {
     return x / y;
 }
+
+function operate(operator, firstNumber, secondNumber) {
+    // Check the operator variable and return the result of the associated function/operation.
+    switch(operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            return divide(firstNumber, secondNumber);
+    }
+}
+
+let firstNumber = null;
+let secondNumber = null;
+let operator = null;
