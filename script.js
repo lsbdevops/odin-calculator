81b4d3f54c6b1a7a7b588573e72516e3680ab179
lsbdevops <lsbdevops@gmail.com>
2023-09-09 14:30:06 +1000

    Add storage and display of operator symbol
    Function added to store the operator button pressed and also display
    the operator symbol to the user. If the operator symbol is pressed
    multiple times, the most recent operator pressed will replace the
    previous - as expected from an calculator.
    After an operator button is pressed, all subsequent numbers pressed
    are stored as the second number in the equation.

--- script.js
+++ script.js
@@ -28,15 +28,36 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 
-function display(character) {
-    document.querySelector(".display").textContent += character;
+function display(character, type) {
+    // Get display element.
+    const display = document.querySelector(".display");
+
+    // If the character is a number, or an operator and an operator is not currently displayed, 
+    // concatenate the character to the displayed text.
+    if ((type === "number") || (type === "operator" && storeFirstNumber)) {
+        display.textContent += character;
+    }
+    // Otherwise, the character is an operator and needs to 'overwrite' the currently displayed operator.
+    else {
+        display.textContent = display.textContent.slice(0, -1) + character;
+    }
 }
 
 function storeNumber(event) {
-    number = event.target.dataset.number;
+    const number = event.target.dataset.number;
+    // Confirm a character was returned, update display and return value.
     if (number) {
-        display(number);
+        display(number, "number");
         return number;
+    }
+}
+
+function storeOperator(event) {
+    const operator =  event.target.dataset.operator;
+    // Confirm a character was returned, update display and return value.
+    if (operator) {
+        display(operator, "operator");
+        return operator;
     }
 }
 
@@ -46,8 +67,8 @@ let operator = "";
 let storeFirstNumber = true;
 
 // Create an event listener for all number buttons on the calculator.
-const buttons = document.querySelectorAll(".number.button");
-buttons.forEach((button) => {
+const numberButtons = document.querySelectorAll(".number.button");
+numberButtons.forEach((button) => {
     button.addEventListener("click", (event) => {
         if (storeFirstNumber) {
             firstNumber += storeNumber(event);
@@ -55,5 +76,16 @@ buttons.forEach((button) => {
         else {
             secondNumber += storeNumber(event);
         }
+    })
+})
+
+// Create an event listener for all operator buttons on the calculator.
+const operatorButtons = document.querySelectorAll(".operator.button");
+operatorButtons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+        operator = storeOperator(event);
+
+        // Update boolean so the second number will be stored.
+        storeFirstNumber = false;
     })
 })
